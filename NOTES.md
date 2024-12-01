b256a3b9b4b7e52dae9cef17372669da9fee3a66
opussf <github@zz9-za.com>
2024-11-30 22:42:53 -0800

    Update to argparse, start a class

---
+++ NOTES.md
@@ -0,0 +1,14 @@
+# Ideas:
+
+## https://wowup.io/
+
+Note: Found this AFTER I thought I would start this project.
+
+Oh well.
+
+## Startup
+use a config file in ~/.addon_updater/  to save settings.
+
+- Sites to check   (curseforge, https://addons.wago.io/)
+- WoW Location(s)
+
--- addon_updater.py
+++ addon_updater.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python3
 
-from optparse import OptionParser
+from argparse import ArgumentParser
 import os
 
+class WoWInstance:
+	""" WoWInstance Class
+	This class takes a path, confirms the path, exposes a few helper functions.
+	"""
+	def __init__(self,path):
+		self.path = path
+
+
+
 if __name__ == "__main__":
-	parser = OptionParser()
-	parser.add_option( "-p", "--path", action="store", type="string", dest="wowpath", default="/Applications/World of Warcraft/",
+	parser = ArgumentParser(description="WoW Addon Updater version ")
+
+	parser.add_argument( "-p", "--path", dest="wowpath", nargs="*", metavar="PATH", default=["/Applications/World of Warcraft/"],
 			help="Path to look for addons.")
 
-	(options, args) = parser.parse_args()
+	options = parser.parse_args()
 
-	print( "Warcraft path: %s" % (options.wowpath, ) )
+	for wowpath in options.wowpath:
+		print( wowpath )
 
-	print( os.path.exists( options.wowpath ) )
+		print( "Warcraft path: %s" % (wowpath, ) )
+
+		print( os.path.exists( wowpath ) )
